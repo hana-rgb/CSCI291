5f988d7ed14c94cb1f74062ceb6d327f3e6b42b3
hana-rgb <hfbe814@uowmail.edu.au>
2024-09-27 12:49:20 +0400

    Add files via upload

--- coffemachine.c
+++ coffemachine.c
@@ -1,3 +1,18 @@
 #include<stdio.h>
 #define espresso_price 3.5
-#define cappuccino_price 
+#define cappuccino_price 4.5
+#define mocha_price 5.5
+#define espresso_grams 8
+#define espresso_water 30
+#define espresso_chocolate 0
+#define cappuccino_grams 8
+#define cappuccino_water 30
+#define cappuccino_milk 70
+#define cappuccino_chocolate 0
+#define mocha_grams 8
+#define mocha_water 39
+#define mocha_milk 160
+#define mocha_chocolate 30
+
+int beans=100 water=50 milk=400 chocolate=60
+total_sales=0;
